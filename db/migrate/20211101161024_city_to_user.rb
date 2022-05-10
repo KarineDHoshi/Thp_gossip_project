e01db1dc366b25236bd64413e935774d0716ebc4
KarineDHoshi <103076747+KarineDHoshi@users.noreply.github.com>
2022-05-10 04:12:16 +0200

    Delete db directory

--- db/migrate/20211101161024_city_to_user.rb
+++
@@ -1,6 +0,0 @@
-class CityToUser < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :users, :city, foreign_key: true
-
-  end
-end
--- db/migrate/20211101161154_join_user_to_table.rb
+++
@@ -1,6 +0,0 @@
-class JoinUserToTable < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :gossips, :user, foreign_key: true
-
-  end
-end
